3e990416cc341bad904760162b5a5a239e506713
alexandra.nanu15 <alexandra.nanu15@gmail.com>
2022-01-29 14:25:26 +0200

    pub-sub

---
+++ mqtt.py
@@ -0,0 +1,22 @@
+from paho.mqtt import client as mqtt_client
+import random
+
+broker = 'broker.emqx.io'
+port = 1883
+topic = "/python/mqtt"
+client_id = f'python-mqtt-{random.randint(0, 1000)}'
+# username = 'emqx'
+# password = 'public'
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+    # Set Connecting Client ID
+    client = mqtt_client.Client(client_id)
+    client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
